dd48a67a1884615cea937c70d0abb713bb8b9486
HaoTsun Lin <htlin@HaoTsundeMacBook-Pro.local>
2016-10-11 17:38:47 +0800

    sign up test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Etu-eDM
+
+<!-- test on chrome, remeber to run 
+	"open /Applications/Google\ Chrome.app/ --args --allow-file-access-from-files" 
+	before testing-->
--- javascripts/controller.js
+++ javascripts/controller.js
@@ -2,7 +2,7 @@ var app=angular.module('mainApp', ['ngRoute']);
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
-		templateUrl: 'login.html'
+		templateUrl: 'html/login.html'
 	})
 	.when('/dashboard',{
 		// resolve: {
@@ -12,20 +12,10 @@ app.config(function($routeProvider){
 		// 		}
 		// 	}
 		// },
-		// templateUrl: 'dashboard.html'
-		
-		// function($location, $rootScope){
-			if (true) {
-				// $location.path('/');
-				redirectTo:'/';
-
-			},
-			// else{
-			// 	templateUrl: 'dashboard.html';
-			// }
-		// },
-		// redirectTo:'/',
-		templateUrl: 'dashboard.html'
+		templateUrl: 'html/dashboard.html'
+	})
+	.when('/signup',{
+		templateUrl: 'html/signup.html'
 	})
 	.otherwise({
 		redirectTo: '/'
@@ -34,17 +24,65 @@ app.config(function($routeProvider){
 
 app.controller('loginCtrl', function($scope,$location,$rootScope){
 	$scope.submit=function(){
-		var uname=$scope.username;
-		var password=$scope.password;
-		//
-		$rootScope.loggedIn=false;
-		//
+		// var uname=$scope.username;
+		// var password=$scope.password;
+		// //
+		// $rootScope.loggedIn=false;
+		// //
 		if($scope.username=='admin' && $scope.password=='admin'){
-			$rootScope.loggedIn=true;
+			//$rootScope.loggedIn=true;
 			$location.path('/dashboard');
 		}
 		else{
-			alert('Oh shit!');
+			alert('Something wrong!');
 		}
 	};
 });
+
+app.controller('signupCtrl', function($scope,$location){
+	$scope.submit=function(){
+		$location.path('/signup');
+	};
+});
+
+app.controller('registerCtrl',['$scope','$http', function($scope,$http){
+	
+	$scope.submit=function(){
+		var accountData={
+			"email": $scope.email,
+			"username": $scope.username,
+			"password": $scope.password
+		};
+		$http.post('json/accounts.json').success(function(data){
+			data.push(accountData);
+		});	
+	}
+
+
+	// var store=this;
+	// //this.account={};
+	// var accountData={
+	// 	"email": $scope.email,
+	// 	"username": $scope.username,
+	// 	"password": $scope.password
+	// };
+
+	// this.addAccount=function(account){
+	// 	$http.post('/json/accounts.json').then(function(data){
+	// 		store.accounts.push(data);
+	// 	});
+	// };
+}]);
+
+app.controller('accountCtrl', ['$http', function($http){
+	var acc = this;
+	acc.accounts = [];
+	$http.get('json/accounts.json').success(function(data){
+		acc.accounts=data;
+	});
+}]);
+
+
+
+
+

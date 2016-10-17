0d0782c6c8aab2c035d977ac27f69cfdc5b3f854
HaoTsun Lin <asnif24@gmail.com>
2016-10-17 10:00:10 +0800

    panelCtrl

--- javascripts/controller.js
+++ javascripts/controller.js
@@ -14,23 +14,9 @@ app.config(function($routeProvider){
 		// },
 		templateUrl: 'html/dashboard.html'
 	})
-	.when('/signup',{
-		templateUrl: 'html/signup.html'
-	})
-	//
-	.when('/test',{
-		templateUrl: 'html/test.html'
-	})
-	.when('/task_eDM',{
-		templateUrl: 'html/task_eDM.html'
-	})
-	.when('/task_SMS',{
-		templateUrl: 'html/task_SMS.html'
-	})
-	.when('/result',{
-		templateUrl: 'html/result.html'
-	})
-	//
+	// .when('/signup',{
+	// 	templateUrl: 'html/signup.html'
+	// })
 	.otherwise({
 		redirectTo: '/'
 	});
@@ -40,11 +26,11 @@ app.controller('loginCtrl', function($scope,$location,$rootScope){
 	$scope.submit=function(){
 		// var uname=$scope.username;
 		// var password=$scope.password;
-		// //
+
 		// $rootScope.loggedIn=false;
-		// //
+
 		if($scope.username=='admin' && $scope.password=='admin'){
-			//$rootScope.loggedIn=true;
+			// $rootScope.loggedIn=true;
 			$location.path('/dashboard');
 		}
 		else{
@@ -59,27 +45,27 @@ app.controller('signupCtrl', function($scope,$location){
 	};
 });
 
-app.controller('registerCtrl',['$scope','$http', function($scope,$http){
-	$scope.submit=function(){
-		var accountData={
-			"email": $scope.email,
-			"username": $scope.username,
-			"password": $scope.password
-		};
-		console.log(accountData);
-		console.log(5566);
-		$http.post('json/accounts',JSON.stringify(accountData))
-		.success(function(data){
-			//data.push(accountData);
-			console.log("success");
-			console.log(data);
-			$scope.msg = 'Data saved';
-		})
-		.error(function(data){
-			console.log("error");
-			console.log(data);
-		});	
-	};
+// app.controller('registerCtrl',['$scope','$http', function($scope,$http){
+// 	$scope.submit=function(){
+// 		var accountData={
+// 			"email": $scope.email,
+// 			"username": $scope.username,
+// 			"password": $scope.password
+// 		};
+// 		console.log(accountData);
+// 		console.log(5566);
+// 		$http.post('json/accounts',JSON.stringify(accountData))
+// 		.success(function(data){
+// 			//data.push(accountData);
+// 			console.log("success");
+// 			console.log(data);
+// 			$scope.msg = 'Data saved';
+// 		})
+// 		.error(function(data){
+// 			console.log("error");
+// 			console.log(data);
+// 		});	
+// 	};
 
 
 	// var store=this;
@@ -95,24 +81,51 @@ app.controller('registerCtrl',['$scope','$http', function($scope,$http){
 	// 		store.accounts.push(data);
 	// 	});
 	// };
-}]);
+// }]);
 
-app.controller('accountCtrl', ['$scope','$http', function($scope,$http){
-	$http.get('json/accounts.json').success(function(data){
-		// console.log(data);
-		$scope.accounts=data;
-	});
-}]);
+// app.controller('accountCtrl', ['$scope','$http', function($scope,$http){
+// 	$http.get('json/accounts.json').success(function(data){
+// 		// console.log(data);
+// 		$scope.accounts=data;
+// 	});
+// }]);
 
-app.controller("panelCtrl", function(){
-	this.tab=1;
-	this.selectTab=function(setTab){
+// for panel control
+app.controller("panelCtrl", function($scope){
+	$scope.tab=1;
+	$scope.selectTab=function(setTab){
 		this.tab=setTab;
 	};
-	this.isSelected=function(checkTab){
-		return true;//this.tab==checkTab;
+	$scope.isSelected=function(checkTab){
+		return this.tab==checkTab;
 	};
 });
+
+app.directive("taskEdm", function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/task_eDM.html"
+	};
+});
+
+app.directive("taskSms", function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/task_SMS.html"
+	};
+});
+
+app.directive("taskResult", function(){
+	return{
+		restrict:"E",
+		templateUrl:"html/result.html"
+	};
+});
+
+
+
+
+
 //python check program
 app.controller("pyCtrl", ['$scope', '$http', function($scope, $http){
 	$scope.submit=function(){
